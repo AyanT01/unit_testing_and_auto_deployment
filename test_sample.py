4a39363dc43c99dfd5bb4fdb6ed2b468ac36971a
AyanT01 <abdulhaqayantayo@gmail.com>
2024-07-16 15:20:43 -0400

    Added unit tests

---
+++ test_sample.py
@@ -0,0 +1,13 @@
+def add(a:int, b:int):
+    '''Function to add two numbers'''
+    return a + b
+
+
+def test_add_positive_integers():
+    assert add(1,2) == 3
+
+def test_add_zeros():
+    assert(add(0,0)) == 0
+
+def test_add_negative_integers():
+    assert(add(-5,-2)) == -7
