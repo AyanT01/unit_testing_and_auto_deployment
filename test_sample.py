d2a1204432dc7dd1fedf8e72a06bd715115bc5b9
AyanT01 <abdulhaqayantayo@gmail.com>
2024-07-16 15:54:52 -0400

    Updated tests

--- test_sample.py
+++ test_sample.py
@@ -2,13 +2,15 @@ def add(a:int, b:int):
     '''Function to add two numbers'''
     return a + b
 
-def do():
-    pass
+
 def test_add_positive_integers():
     assert add(1,2) == 3
 
 def test_add_zeros():
-    assert(add(0,0)) == 0
+    assert add(0,0) == 0
 
 def test_add_negative_integers():
-    assert(add(-5,-2)) == -7
+    assert add(-5,-2) == -7
+
+def wrong_test():
+    assert add(2,3) == 6
